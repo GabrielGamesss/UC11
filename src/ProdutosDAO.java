df4bdbfaa5b9fdda814a500312fb7d51a16b8c5a
GabrielGamesss <gabecoelhoail@gmail.com>
2026-06-08 21:19:48 -0300

    Botões Vender e consultar vendas

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -92,7 +92,38 @@ public class ProdutosDAO {
         return listagem;
     }
     
-    
+    public void venderProduto(int id){
+
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+
+        prep.setInt(1, id);
+
+        int linhasAfetadas = prep.executeUpdate();
+
+        if(linhasAfetadas > 0){
+            JOptionPane.showMessageDialog(null,
+                    "Produto vendido com sucesso!");
+        }else{
+            JOptionPane.showMessageDialog(null,
+                    "Produto não encontrado!");
+        }
+
+        prep.close();
+        conn.close();
+
+    } catch (Exception erro){
+
+        JOptionPane.showMessageDialog(null,
+                "Erro ao vender produto: " + erro.getMessage());
+
+    }
+  }   
     
         
 }
